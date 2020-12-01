cdf3c9ea08a241c137b90c84a863261fc3d0c990
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-12-01 20:36:01 +0100

    Añade nueva URI con parametro

--- express/miniapi.js
+++ express/miniapi.js
@@ -2,12 +2,24 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 8080
 
+let nombre = 'anónimo'
+
 app.get('/', (req, res) => {
     res.send({ message: 'Funciona :D' })
 })
 
 app.get('/status', (req, res) => {
     res.send({ status: 'Healthy' })
+})
+
+app.put('/saluda/:nombre', (req, res) => {
+    nombre = req.params.nombre
+
+    res.send(`Te llamas ${nombre}`)
+})
+
+app.get('/saluda', (req, res) => {
+    res.send(`Hola, ${nombre}`)
 })
 
 app.listen(port, () => {
